14df94c4738816d3fe8fc5e820fa695ee9a46550
Stephan Plepelits <skunk@xover.mud.at>
2012-06-17 06:22:51 +0200

    Initial commit

---
+++ modulekit.php
@@ -0,0 +1,16 @@
+<?php
+$name="AJAX";
+
+// an ID to identify this module
+$id="modulekit-ajax";
+
+// these modules should be loaded first
+$depend=array();
+
+// these files will be included in this order:
+$include_php=array(
+);
+$include_js=array(
+);
+$include_css=array(
+);
